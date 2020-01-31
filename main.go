37d80302f7f0041f694566c9489469995b06b1b0
Takamichi Omori <merrytakamerry@gmail.com>
2020-02-01 02:53:31 +0900

    fix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 var COUNT = 10
@@ -24,9 +25,25 @@ func dangerousHandler(w http.ResponseWriter, r *http.Request) {
 	COUNT = COUNT - 1
 	s := strconv.Itoa(COUNT)
 	if s == "0" {
-		// fmt.Fprintf(w, "DIED")
 		os.Exit(1)
 	}
 
+	fmt.Fprintf(w, "<h1>"+s+" TIMES UNTIL DIE</h1>")
+}
+
+func fixedDangerousHandler(w http.ResponseWriter, r *http.Request) {
+	if COUNT == 0 {
+		fmt.Fprintf(w, "<h1>HEALING DANGEROUS POINT</h1>")
+		return
+	}
+	COUNT = COUNT - 1
+	s := strconv.Itoa(COUNT)
+	if s == "0" {
+		// fmt.Fprintf(w, "DIED")
+		go func() {
+			time.Sleep(10 * time.Second)
+			COUNT = 10
+		}()
+	}
 	fmt.Fprintf(w, "<h1>"+s+" TIMES UNTIL DIE</h1>")
 }
